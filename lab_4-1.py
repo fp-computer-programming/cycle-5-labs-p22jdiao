be07a3120dd99024a35e3190cc79d3afdf5ff981
p22jdiao <p22jdiao@fairfieldprep.org>
2021-10-26 10:14:07 -0400

    4-1

---
+++ lab_4-1.py
@@ -0,0 +1,28 @@
+# Author: JD 10/26/2021
+
+print("Fairfield "+"Prep")
+
+print("-"*20)
+
+sentence = "I'm loving this short story I'm reading, "+'"The Fall of the House of Usher"'
+print(sentence)
+
+print(len(sentence))
+
+# x = 0
+# string = "Apple"
+# while x != len(string):
+#     if string[x] == "l":
+#         print(x)
+#         break
+#     x += 1
+
+
+# string = "Apple"
+# print(string[3])
+
+# n = 0
+# for e in sentence:
+#     n += 1
+
+# print(n)
