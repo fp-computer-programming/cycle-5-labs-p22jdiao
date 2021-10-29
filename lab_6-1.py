f012fe7454339d469db1f6aba1fd65e081d9448f
p22jdiao <p22jdiao@fairfieldprep.org>
2021-10-29 10:46:22 -0400

    6-2, 6-1

---
+++ lab_6-1.py
@@ -0,0 +1,11 @@
+# Author: JD 10/29/2021
+
+string = "flibbertigibbet"
+
+a = string.find("t")
+
+print(a)
+
+b = "I wish, I wish, I was a fish.".split()
+
+print(b)
---
+++ lab_6-2.py
@@ -0,0 +1,9 @@
+# Author: JD 10/29/2021
+
+first_name = input("First person's name: ")
+
+second_name = input("Second person's name: ")
+
+output = "Hello, {0}. My name is {1}.".format(first_name, second_name)
+
+print(output)
